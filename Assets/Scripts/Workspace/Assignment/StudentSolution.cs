5a9cd0be1319fe5c46499fc2d7b572b312056eae
Kenluckyz <aoken0991242405@gmail.com>
2026-09-24 19:42:34 +0700

    FinishAssignment

--- Assets/Scripts/Workspace/Assignment/StudentSolution.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution.cs
@@ -12,62 +12,64 @@ namespace Assignment
         #region Lecture
         public int[] LCT01_SelectionSortAscending(int[] numbers)
         {
-            for (int i = 0; i < numbers.Length - 1; i++)
+            int[] sortedNumbers = (int[])numbers.Clone();
+
+            for (int i = 0; i < sortedNumbers.Length - 1; i++)
             {
                 int minIndex = i;
-                for (int j = i + 1; j < numbers.Length; j++)
+                for (int j = i + 1; j < sortedNumbers.Length; j++)
                 {
-                    if (numbers[j] < numbers[minIndex])
+                    if (sortedNumbers[j] < sortedNumbers[minIndex])
                     {
                         minIndex = j;
                     }
                 }
 
-                //int temp = numbers[minIndex];
-                //numbers[minIndex] = numbers[i];
-                //numbers[i] = temp;
-                (numbers[i], numbers[minIndex]) = (numbers[minIndex], numbers[i]);
+                int temp = sortedNumbers[i];
+                sortedNumbers[i] = sortedNumbers[minIndex];
+                sortedNumbers[minIndex] = temp;
 
             }
-            return numbers;
+            return sortedNumbers;
         }
 
         public int[] LCT02_BubbleSortAscending(int[] numbers)
 
         {
-            for (int i = 0; i < numbers.Length - 1; i++)
+            int[] sortedNumbers = (int[])numbers.Clone();
+
+            for (int i = 0; i < sortedNumbers.Length - 1; i++)
             {
-                bool swapped = false;
-                for (int j = 0; j < numbers.Length - i - 1; j++)
+                for (int j = 0; j < sortedNumbers.Length - i - 1; j++)
 
                 {
 
-                    if (numbers[j] > numbers[j + 1])
+                    if (sortedNumbers[j] > sortedNumbers[j + 1])
                     {
-                        (numbers[j], numbers[j + 1]) = (numbers[j + 1], numbers[j]);
-                        swapped = true;
+                        int temp = sortedNumbers[j];
+                        sortedNumbers[j] = sortedNumbers[j + 1];
+                        sortedNumbers[j + 1] = temp;
                     }
                 }
-
-                if (!swapped)
-                    break;
             }
-            return numbers;
+            return sortedNumbers;
         }
         public int[] LCT03_InsertionSortAscending(int[] numbers)
         {
-            for (int i = 1; i < numbers.Length; i++)
+            int[] sortedNumbers = (int[])numbers.Clone();
+
+            for (int i = 1; i < sortedNumbers.Length; i++)
             {
-                int key = numbers[i];
+                int key = sortedNumbers[i];
                 int j = i - 1;
-                while (j >= 0 && numbers[j] > key)
+                while (j >= 0 && sortedNumbers[j] > key)
                 {
-                    numbers[j + 1] = numbers[j];
+                    sortedNumbers[j + 1] = sortedNumbers[j];
                     j--;
                 }
-                numbers[j + 1] = key;
+                sortedNumbers[j + 1] = key;
             }
-            return numbers;
+            return sortedNumbers;
         }
 
         #endregion
@@ -76,21 +78,87 @@ namespace Assignment
 
         public int[] AS01_SelectionSortDescending(int[] numbers)
         {
-            return numbers;
+            int[] sortedNumbers = (int[])numbers.Clone();
+
+            for (int i = 0; i < sortedNumbers.Length - 1; i++)
+            {
+                int maxIndex = i;
+                for (int j = i + 1; j < sortedNumbers.Length; j++)
+                {
+                    if (sortedNumbers[j] > sortedNumbers[maxIndex])
+                    {
+                        maxIndex = j;
+                    }
+                }
+
+                int temp = sortedNumbers[i];
+                sortedNumbers[i] = sortedNumbers[maxIndex];
+                sortedNumbers[maxIndex] = temp;
+            }
+
+            return sortedNumbers;
         }
 
         public int[] AS02_BubbleSortDescending(int[] numbers)
         {
-            return numbers;
+            int[] sortedNumbers = (int[])numbers.Clone();
+
+            for (int i = 0; i < sortedNumbers.Length - 1; i++)
+            {
+                for (int j = 0; j < sortedNumbers.Length - i - 1; j++)
+                {
+                    if (sortedNumbers[j] < sortedNumbers[j + 1])
+                    {
+                        int temp = sortedNumbers[j];
+                        sortedNumbers[j] = sortedNumbers[j + 1];
+                        sortedNumbers[j + 1] = temp;
+                    }
+                }
+            }
+
+            return sortedNumbers;
         }
 
         public int[] AS03_InsertionSortDescending(int[] numbers)
         {
-            return numbers;
+            int[] sortedNumbers = (int[])numbers.Clone();
+
+            for (int i = 1; i < sortedNumbers.Length; i++)
+            {
+                int key = sortedNumbers[i];
+                int j = i - 1;
+
+                while (j >= 0 && sortedNumbers[j] < key)
+                {
+                    sortedNumbers[j + 1] = sortedNumbers[j];
+                    j--;
+                }
+
+                sortedNumbers[j + 1] = key;
+            }
+
+            return sortedNumbers;
         }
 
         public int AS04_FindTheSecondLargestNumber(int[] numbers)
         {
+            if (numbers.Length < 2)
+            {
+                return 0;
+            }
+
+            int[] sortedNumbers = (int[])numbers.Clone();
+            System.Array.Sort(sortedNumbers);
+
+            int largestNumber = sortedNumbers[sortedNumbers.Length - 1];
+            for (int i = sortedNumbers.Length - 2; i >= 0; i--)
+            {
+                if (sortedNumbers[i] < largestNumber)
+                {
+                    return sortedNumbers[i];
+                }
+            }
+
             return 0;
         }
 
@@ -100,7 +168,41 @@ namespace Assignment
 
         public int EX01_FindLongestConsecutiveSequence(int[] numbers)
         {
-            return 0;
+            if (numbers.Length == 0)
+            {
+                return 0;
+            }
+
+            int[] sortedNumbers = (int[])numbers.Clone();
+            System.Array.Sort(sortedNumbers);
+
+            int currentLength = 1;
+            int longestLength = 1;
+
+            for (int i = 1; i < sortedNumbers.Length; i++)
+            {
+                if (sortedNumbers[i] == sortedNumbers[i - 1])
+                {
+                    continue;
+                }
+
+                if ((long)sortedNumbers[i] == (long)sortedNumbers[i - 1] + 1)
+                {
+                    currentLength++;
+                }
+                else
+                {
+                    currentLength = 1;
+                }
+
+                if (currentLength > longestLength)
+                {
+                    longestLength = currentLength;
+                }
+            }
+
+            Debug.Log("The longest consecutive sequence is: " + longestLength);
+            return longestLength;
         }
 
         #endregion
